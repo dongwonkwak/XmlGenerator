2f4a2d7d1e7fb4c27d90a8aa8423b39e4ccb63c0
dongwon.kwak <dongwon.kwak@somansa.com>
2020-08-10 14:56:14 +0900

    first commit

---
+++ XmlGenerator/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+/*
+	Foo("Hello", 123, 123.4, "Hi");
+
+	<Foo>
+		<String>Hello</String>
+		<Integer>123</Integer>
+		<Float>123.4</Float>
+		<String>Hi</Hi>
+	</Foo>
+
+
+	MyClass myclass;
+
+	Foo("Hello", 123, 123.4, "Hi", myclass);
+	// Compile Error. Unsupported Ty[e
+*/
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
